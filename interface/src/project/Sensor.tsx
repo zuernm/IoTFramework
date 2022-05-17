c9277175490fbf1a6c607da59b0f19e912607021
Zeus <zuern.markus@gmail.com>
2022-05-17 21:03:34 +0200

    update: apache echarts for react

--- interface/src/project/Sensor.tsx
+++ interface/src/project/Sensor.tsx
@@ -4,15 +4,70 @@ import { Typography, Box, List, ListItem, ListItemText } from '@mui/material';
 
 import GaugeChart from 'react-gauge-chart';
 
+import ReactECharts from 'echarts-for-react';
+
 import { SectionContent } from '../components';
 
-const Sensor: FC = () => (
-    <GaugeChart id="gauge-chart2"
-                nrOfLevels={20}
-                percent={0.86}
-                hideText={true}
-                formatTextValue={value => value + 'kbit/s'}
-    />
-);
+const Sensor: FC = () => {
+    const options = {
+        series: [
+            {
+                type: 'gauge',
+                min: 400,
+                max: 1600,
+                splitNumber: 12,
+                axisLine: {
+                    lineStyle: {
+                        width: 30,
+                        color: [
+                            [0.333, '#47f184'],
+                            [0.666, '#e3f65a'],
+                            [1, '#eb3d3d']
+                        ]
+                    }
+                },
+                pointer: {
+                    itemStyle: {
+                        color: 'auto'
+                    }
+                },
+                axisTick: {
+                    distance: -30,
+                    length: 8,
+                    lineStyle: {
+                        color: '#fff',
+                        width: 2
+                    }
+                },
+                splitLine: {
+                    distance: -30,
+                    length: 30,
+                    lineStyle: {
+                        color: '#fff',
+                        width: 4
+                    }
+                },
+                axisLabel: {
+                    color: 'auto',
+                    distance: 40,
+                    fontSize: 16
+                },
+                detail: {
+                    valueAnimation: true,
+                    formatter: '{value} ppm',
+                    color: 'auto',
+                    fontSize: 26
+                },
+                data: [
+                    {
+                        value: 1000
+                    }
+                ]
+            }
+        ]
+    };
+
+    return <ReactECharts option={options} style={{height: '500px', width: '100%'}}/>;
+};
 
 export default Sensor;
